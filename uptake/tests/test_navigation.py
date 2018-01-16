20a0f8fa19ec114043a1e15593eb5983ec1548ed
saumark.nemani <saumark88@gmail.com>
2018-01-15 20:23:50 -0600

    -Updated steps comments to the tests

--- uptake/tests/test_navigation.py
+++ uptake/tests/test_navigation.py
@@ -1,25 +1,34 @@
 
 def test_navigate_from_home_to_industries(get_pom):
+    # 1. Creating an object of the Page object model class
     pom_obj = get_pom
+    # 2. Getting the page object of teh industries page
     pom_obj.get_industries_page()
 
+    # 3. Verifying the header of the Industries page
     assert pom_obj.page_obj.verify_header("Industries")
 
+    # 4. Navigating back to the home page
     pom_obj.get_home_page()
-    pom_obj.page_obj.set_home()
 
+    # 5. Verifying the heading of the home page
     assert pom_obj.page_obj.verify_header("Uptake Products")
 
 
 def test_navigate_from_home_to_products(get_pom):
+    # 1. Creating an object of the Page object model class
     pom_obj = get_pom
+    # 2. Getting the page object of the products page
     pom_obj.get_products_page()
 
+    # 3. Verifying the header of the Products page
     assert pom_obj.page_obj.verify_header("Products")
 
+    # 4. Navigating back to the home page
     pom_obj.get_home_page()
     pom_obj.page_obj.set_home()
 
+    # 5. Verifying the heading of the home page
     assert pom_obj.page_obj.verify_header("Uptake Products")
 
 
